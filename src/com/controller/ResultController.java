39b2867d0b24ad2e502a2982d3df07a1706c0469
Viktor Lalev <viktor.lalev@donatix.net>
2019-07-10 12:52:10 +0300

    Add Pie example

--- src/com/controller/ResultController.java
+++ src/com/controller/ResultController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.primefaces.event.FlowEvent;
 import org.primefaces.model.chart.PieChartModel;
+import org.primefaces.model.charts.ChartData;
 
 import com.database.ResultQuery;
 import com.voting.Candidate;
@@ -29,7 +30,13 @@ public class ResultController implements Serializable {
 	private Integer candidate_id;
 	private Integer votes;
 
-	public ResultController() {
+	private PieChartModel pieModel;
+
+	@PostConstruct
+	public void init() {
+
+		createPieModel();
+
 	}
 
 	public Integer getCandidate_id() {
@@ -47,6 +54,23 @@ public class ResultController implements Serializable {
 
 	public void setList(List<Result> list) {
 		this.list = list;
+	}
+
+	public PieChartModel getPieModel() {
+		return pieModel;
+	}
+
+	private void createPieModel() {
+		pieModel = new PieChartModel();
+
+		pieModel.set("Brand 1", 540);
+		pieModel.set("Brand 2", 325);
+		pieModel.set("Brand 3", 702);
+		pieModel.set("Brand 4", 421);
+
+		pieModel.setTitle("Simple Pie");
+		pieModel.setLegendPosition("w");
+		pieModel.setShadow(false);
 	}
 
 }
